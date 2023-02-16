aff3a4e72552bfaf93d82b3b9bed149faacbb21c
Malvika Sharma <sharma.malvika2193@gmail.com>
2023-02-16 23:33:30 +0530

    final commit:- handled incorrect city name

--- src/App.js
+++ src/App.js
@@ -2,63 +2,80 @@ import React, { useState } from "react";
 import axios from "axios";
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState(null);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=48d3607ae8907b2e2d9eab74db18eab0`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
+      axios
+        .get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+          setError(null);
+        })
+        .catch(setError);
       setLocation("");
     }
   };
+
   return (
     <div className="app">
       <div className="search">
         <input
           value={location}
-          onChange={event => setLocation(event.target.value)}
+          onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
           placeholder="Enter Location"
-          type="text"/>
+          type="text"
+        />
       </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp"> 
-            {data.main ? <h1>{data.main.temp}&deg;C</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+
+      {error && (
+        <div className="container">
+          <p className="bold">Please enter correct city name.</p>
         </div>
+      )}
 
-
-        {data.name != undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold"> {data.main.feels_like}&deg;C</p>
-              ) : null}
-              <p>Feels Like</p>
+      {!error && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p className="bold">{data.name}, {data.sys.country}</p>
             </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp}&deg;C</h1> : null}
             </div>
-            <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
-              <p>Wind Speed</p>
+            <div className="description">
+              {data.weather ? <p className="bold">{data.weather[0].main}</p> : null}
             </div>
           </div>
-        )}
 
-      
-      </div>
+          {data.name != undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold"> {data.main.feels_like}&deg;C</p>
+                ) : null}
+                <p className="bold">Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p className="bold">Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed} MPH</p>
+                ) : null}
+                <p className="bold">Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
